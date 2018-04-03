45d2799617821db1109f1ef43d27ae5aff9c5da8
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-04-03 15:41:43 +0300

    svm decision boundary

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+train_values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+results = eye(64, 3);
+iteration_count = 0;
 
 
+for C_train = train_values
+  for sigma_train = train_values
+    iteration_count += 1;
+    model = svmTrain(X, y, C_train, @(x1, x2) gaussianKernel(x1, x2, sigma_train));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    results(iteration_count, :) = [C_train, sigma_train, prediction_error];
+  endfor
+endfor
 
-
-
+sorted_results = sortrows(results, 3);
+C = sorted_results(1, 1);
+sigma = sorted_results(1, 2)
 
 % =========================================================================
 
