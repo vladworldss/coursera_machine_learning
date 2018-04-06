e57c5535ec8ecba9f3022f2ea875a0f120d08f5a
Vladimir Gerasimenko <vladworldss@yandex.ru>
2018-04-06 17:42:59 +0300

    default ex7

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -27,13 +27,14 @@ train_values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 results = eye(64, 3);
 iteration_count = 0;
 
-
 for C_train = train_values
   for sigma_train = train_values
-    iteration_count += 1;
     model = svmTrain(X, y, C_train, @(x1, x2) gaussianKernel(x1, x2, sigma_train));
+    
     predictions = svmPredict(model, Xval);
     prediction_error = mean(double(predictions ~= yval));
+    
+    iteration_count += 1;
     results(iteration_count, :) = [C_train, sigma_train, prediction_error];
   endfor
 endfor
